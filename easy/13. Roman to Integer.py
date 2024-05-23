13642c7a8dbb9d178fdffe76e172f3a55b70d73d
Ahmed Amer <147430421+ahmedAmer8@users.noreply.github.com>
2024-05-23 15:31:14 +0300

    Update 13. Roman to Integer.py

--- easy/13. Roman to Integer.py
+++ easy/13. Roman to Integer.py
@@ -1,20 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        values = {'I': 1,
-                    'V': 5,
-                    'X': 10,
-                    'L': 50,
-                    'C': 100,
-                    'D': 500,
-                    'M': 1000}
-        result = 0
-        i = 0
-        while i < len(s) -1:
-            if values[s[i]] >= values[s[i+1]]:
-                result += values[s[i]]
-                i += 1
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        ans = 0
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
             else:
-                result += values[s[i+1]] - values[s[i]]
-                i += 2
-        result += values[s[-1]] if i == len(s) - 1 else 0
-        return result
+                ans += m[s[i]]
+        
+        return ans
