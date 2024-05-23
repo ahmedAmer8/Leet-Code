13eacf69048f634e5d30b3f04871aca8376690d6
Ahmed Amer <147430421+ahmedAmer8@users.noreply.github.com>
2024-05-23 12:26:34 +0300

    Update 2. Add Two Numbers.py

--- medium/2. Add Two Numbers.py
+++ medium/2. Add Two Numbers.py
@@ -7,43 +7,15 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, None)
         curr = dummy
-        remainder = 0
-        while l1 and l2:
-            s = l1.val + l2.val + remainder
-            if s > 9:
-                remainder = 1
-                s = s % 10
-            else:
-                remainder = 0
-            new_node = ListNode(s)
-            curr.next = new_node
+        carry = 0
+        while l1 or l2 or carry:
+            val2 = l2.val if l2 else 0
+            val1 = l1.val if l1 else 0
+            s = val1 + val2 + carry
+            carry = s // 10
+            s = s % 10
+            curr.next = ListNode(s)
             curr = curr.next
-            l1 = l1.next
-            l2 = l2.next
-        while l1:
-            s = l1.val + remainder
-            if s > 9:
-                remainder = 1
-                s = s % 10
-            else:
-                remainder = 0
-            new_node = ListNode(s)
-            curr.next = new_node
-            curr = curr.next
-            l1 = l1.next
-        while l2:
-            s = l2.val + remainder
-            if s > 9:
-                remainder = 1
-                s = s % 10
-            else:
-                remainder = 0
-            new_node = ListNode(s)
-            curr.next = new_node
-            curr = curr.next
-            l2 = l2.next
-        if remainder:
-            new_node = ListNode(remainder)
-            curr.next = new_node
-            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         return dummy.next
