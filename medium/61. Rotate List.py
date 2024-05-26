90ba9499fa926ff10625acdf44b16b4927837662
Ahmed Amer <147430421+ahmedAmer8@users.noreply.github.com>
2024-05-26 18:11:13 +0300

    circular solution 61. Rotate List.py

--- medium/61. Rotate List.py
+++ medium/61. Rotate List.py
@@ -10,21 +10,21 @@ class Solution:
             return head
         # find list size
         curr = head
-        size = 0
-        while curr:
+        size = 1
+        while curr.next:
             size += 1
             curr = curr.next
+        # circular linked list
+        curr.next = head
         # there is no need to rotate the list a full cycle as it will be the same
         # so we limit the values of rotation to be between 0 and size - 1
         # by make k = k % size 
         k = k % size
-        while k:
-            curr = head
-            while curr and curr.next.next:
-                curr = curr.next
-            last_node = curr.next
-            curr.next = curr.next.next
-            last_node.next = head
-            head = last_node
-            k -= 1
-        return head
+        # the last node after rotation should point at None
+        # this node can be finde just before the size - k position
+        temp = head
+        for i in range (size - k - 1):
+            temp = temp.next
+        result = temp.next
+        temp.next = None
+        return result
